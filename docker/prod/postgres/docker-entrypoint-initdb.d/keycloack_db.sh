7390f63c52d6c28b62a7bf8c09ecf45c2918141f
Viviani Demetrio <d.viviani@fuzzy-net.it>
2025-05-07 09:17:49 +0200

    Pulizie...

--- docker/prod/postgres/docker-entrypoint-initdb.d/keycloack_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER keycloak WITH PASSWORD 'keycloak';
-	CREATE DATABASE keycloak OWNER keycloak ENCODING 'UTF8' LOCALE_PROVIDER 'icu' ICU_LOCALE 'und' LC_COLLATE 'C.UTF-8' LC_CTYPE 'C.UTF-8' template template0;
-EOSQL
--- docker/prod/postgres/docker-entrypoint-initdb.d/map_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER map WITH PASSWORD 'map';
-	CREATE DATABASE map OWNER map ENCODING 'UTF8' LOCALE_PROVIDER 'icu' ICU_LOCALE 'und' LC_COLLATE 'C.UTF-8' LC_CTYPE 'C.UTF-8' template template0;
-EOSQL
